e7bcf8ec9dfee1453ce8857665b9aa87505a22f8
Timothy <superfluous11@gmail.com>
2018-11-13 06:27:53 -0800

    created put method for bst class

--- bst.js
+++ bst.js
@@ -1,1 +1,22 @@
 const Node = require('./node');
+
+class BST {
+
+  constructor(){
+    this.root = null;
+  }
+
+  put(key, value){
+    this.root = this.putHelper(key, value, this.root);
+  }
+
+  putHelper(key, value, node){
+    if(!node) return new Node(key, value);
+
+    if(key < node.key) node.left = this.putHelper(key, value, node.left);
+    else if(key > node.key) node.right = this.putHelper(key, value, node.right);
+    else node.value = value;
+
+    return node;
+  }
+}
