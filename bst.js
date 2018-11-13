be00d3467573a5f89c0bd663ba7d91bcee0b85ea
Timothy <superfluous11@gmail.com>
2018-11-13 06:34:09 -0800

    created print method on bst class

--- bst.js
+++ bst.js
@@ -31,5 +31,32 @@ class BST {
     else if(key > node.key) return this.getHelper(key, node.right);
     else return node.value;
   }
+
+  print(node = this.root){
+    if(!node) return;
+
+    this.print(node.left);
+    console.log(`${node.key}: ${node.value}`);
+    this.print(node.right);
+  }
+
 }
 
+if(require.main === module){
+
+  const bst = new BST();
+  const input = [3, 1, 4, 6, 9, 2, 5, 7];
+
+  input.forEach(num => {
+    bst.put(num, num);
+  }); 
+  bst.print();
+
+  // console.log('Deleting');
+  // [1, 2, 3, 9 ].forEach(num => bst.remove(num));
+  // bst.print();
+  // console.log(`The height of BST is:`, heightOfBST(bst.root));
+  // console.log(`BST is balanced:`, checkBalanced(bst.root));
+  // console.log(`BST is a binary search tree:`, checkBST(bst.root));
+  // nthLast(bst.root, 3);
+}
